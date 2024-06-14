6a083598527ef9df2fe89b7a4dcdb26eaa376de4
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-06-14 14:28:28 +0100

    envoi

--- script.js
+++ script.js
@@ -9,10 +9,41 @@ fetch("./image/assets/SVG/cloud.svg")
     return response.text();
   })
   .then((svgContent) => {
-    // Injectez le contenu SVG dans l'élément
+   
     svgContainer.innerHTML = svgContent;
     svgContainerr.forEach((e) => (e.innerHTML = svgContent));
   })
   .catch((error) => {
     console.error("Erreur lors du chargement du SVG:", error);
   });
+
+  const t= fetch(`https://api.weatherapi.com/v1/forecast.json?key=ae9ee47e36fe4d658c9234801241306&q=${bejaia}&days=7&aqi=yes`).then(res=>res.json()).then(ress=>console.log(ress));
+ 
+  function validateLocation(location) {
+    // Expression régulière pour vérifier si la chaîne ne contient que des lettres, chiffres, espaces et tirets
+    const regex = /^[a-zA-Z0-9\s-]+$/;
+  
+    
+    return regex.test(location);
+  }
+  
+  const location = 'bejaia'; 
+
+if (!validateLocation(location)) {
+  console.error('Erreur: La localisation contient des caractères non autorisés.');
+} else {
+  fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=7&aqi=yes`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    
+    });
+}
